f2604662ca95fa7bc819c35d8e5edf31ea6fab94
jefersonrodrigal <jefersonrodrigoalmeida@outlook.com>
2024-08-08 18:46:46 -0300

    Primeiro commit - adicionando os arquivos ao repo

---
+++ Controllers/RequestController.cs
@@ -0,0 +1,30 @@
+﻿using APIConsumoExternalApi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace APIConsumoExternalApi.Controllers
+{
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        [HttpGet("/cep/{cep}")]
+        public async Task<IActionResult> GetAddress(string cep)
+        {
+            var url = $"https://viacep.com.br/ws/{cep}/json/";
+            var client = new HttpClient();
+            try
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+                resp.EnsureSuccessStatusCode();
+                var respContent = await resp.Content.ReadAsStringAsync();
+                var respCep = JsonSerializer.Deserialize<CepViewModel>(respContent);
+
+                return Ok(respCep);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
---
+++ ViewModels/CepViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace APIConsumoExternalApi.ViewModels
+{
+    public class CepViewModel
+    {
+        [JsonPropertyName("cep")]
+        public string Cep { get; set; }
+        [JsonPropertyName("logradouro")]
+        public string Logradouro { get; set; }
+        [JsonPropertyName("localidade")]
+        public string Localidade { get; set; }
+    }
+}
